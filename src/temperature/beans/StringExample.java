e8486e3ccc5b231879f2928f82e034949dec498d
reshma-dotcom <reshmam24aid@vetias.ac.in>
2025-07-07 12:04:49 +0530

    words letters characters

--- src/temperature/beans/StringExample.java
+++ src/temperature/beans/StringExample.java
@@ -3,6 +3,7 @@ package temperature.beans;
 public class StringExample {
     public static void main(String[] args) {
        
+
         String firstName  = "reshma";
         String lastName = "musthafa";
         String fullName = firstName .concat(lastName);
@@ -15,8 +16,22 @@ public class StringExample {
        
         String sentance = "hi this is reshma here currently pursuing my ug degree bsc cs aids in vetias";
         String[] words = sentance.split(" ");
+        String[] letters = sentance.split("");
+        String[] characters = sentance.split("\\s+");
         System.out.println(words.length);
+        System.out.println(letters.length);
+        System.out.println(characters.length);
         
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+        for (String letter : letters) {
+            System.out.println(letter);
+        }
+        for (String character : characters) {
+            System.out.println(character);
+        }
     }
     
 }
